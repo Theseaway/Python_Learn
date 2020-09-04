322d7a68b55f3b00b08fb910fade9a3126822bf9
Theseaway <2606621776@qq.com>
2020-09-04 16:06:55 +0800

    成功读取数据，通过正则化表达式得到了相关的数据，并根据re.sub函数将无用信息去除，最后利用分片函数spilt() 和格式转换float() 得到了相关的数值数组，利用库xlwt将数组写入excel表格中。

--- UR-arm.py
+++ UR-arm.py
@@ -1,13 +1,12 @@
-from pathlib import Path
-
 import re
 import os
 import xlwt
 import xlrd
+from pathlib import Path
 
 with open('jointStates.txt','r') as f:
     data=f.readlines()
-#判断有无文件存在，并进行清空操作
+
 if not Path('position.txt').is_file() \
    or not Path('velocity.txt').is_file() \
    or not Path('effort.txt').is_file() :
@@ -21,7 +20,9 @@ fr=open('velocity.txt',"w")
 fr.close()
 fr=open('effort.txt',"w")
 fr.close()
-for i in range(0,len(data)//12 -1):
+maxnum=len(data)//12
+
+for i in range(0,maxnum -1):
     position=data[i*12+8]
     p1="position\:\s\[.*\]"
     pattern1=re.compile(p1)
@@ -31,7 +32,6 @@ for i in range(0,len(data)//12 -1):
     fr=open('position.txt',"a")
     fr.write(pos_val+'\n')
     fr.close()
-    print(pos_val)
 
     velocity=data[i*12+9]
     p2="velocity\:\s\[.*\]"
@@ -42,7 +42,6 @@ for i in range(0,len(data)//12 -1):
     fr=open('velocity.txt',"a")
     fr.write(vel_val+'\n')
     fr.close()
-    print(vel_val)
 
     effort=data[i*12+10]
     p3="effort\:\s\[.*\]"
@@ -53,9 +52,17 @@ for i in range(0,len(data)//12 -1):
     fr=open('effort.txt',"a")
     fr.write(eft_val+'\n')
     fr.close()
-    print(eft_val)
 
 my_Workbook=xlwt.Workbook(encoding='utf-8')
-position=my_Workbook.add_sheet('position information')
+sheet_position=my_Workbook.add_sheet('position_information')
 
-pos_val.count
+with open('position.txt') as File1:
+    position_info=File1.readlines()
+array=position_info[0].split(',')
+column=len(array)
+for i in range(0,maxnum-1):
+    temp=position_info[i].split(',')
+    for j in range(0,column):
+        posnum=float(temp[j])
+        sheet_position.write(i,j,posnum)
+my_Workbook.save('position.xls')
